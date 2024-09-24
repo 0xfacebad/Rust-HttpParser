b60460da8056194642cb9cb32713566e5fe9e598
yasir378 <61603180+yasir378@users.noreply.github.com>
2024-09-24 23:28:21 +0530

    Delete target/debug/deps/preferces-271b9804d434661f.d

--- target/debug/deps/preferces-271b9804d434661f.d
+++
@@ -1,5 +0,0 @@
-/home/luna/Downloads/preferces/target/debug/deps/preferces-271b9804d434661f: src/main.rs
-
-/home/luna/Downloads/preferces/target/debug/deps/preferces-271b9804d434661f.d: src/main.rs
-
-src/main.rs:
